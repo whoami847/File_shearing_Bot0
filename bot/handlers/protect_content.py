fd64151331951265ec8748657c123a08ff32d4be
whoami847 <mrbeastbangla069@gmail.com>
2025-04-04 23:25:29 +0600

    Create protect_content.py

---
+++ bot/handlers/protect_content.py
@@ -0,0 +1,8 @@
+from fastapi import Request
+
+async def content_protection_middleware(request: Request, call_next):
+    response = await call_next(request)
+    response.headers["X-Content-Type-Options"] = "nosniff"
+    response.headers["X-Frame-Options"] = "DENY"
+    response.headers["X-XSS-Protection"] = "1; mode=block"
+    return response
