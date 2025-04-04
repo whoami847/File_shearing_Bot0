3ba1263bf7682a54aaf806caf961027f4cd911b4
whoami847 <mrbeastbangla069@gmail.com>
2025-04-04 23:37:31 +0600

    Delete bot/handlers/utils directory

--- bot/handlers/utils/buttons.py
+++
@@ -1,7 +0,0 @@
-from typing import List, Dict
-
-def generate_buttons(button_config: List[Dict[str, str]]):
-    return [
-        {"text": btn["text"], "callback_data": btn["action"]}
-        for btn in button_config
-    ]
--- bot/handlers/utils/encoder.py
+++
@@ -1,7 +0,0 @@
-import base64
-
-def encode_base64(data: str) -> str:
-    return base64.b64encode(data.encode()).decode()
-
-def decode_base64(data: str) -> str:
-    return base64.b64decode(data).decode()
