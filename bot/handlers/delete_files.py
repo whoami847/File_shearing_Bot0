ff9bbc9670f1fd45e1e424ac2ae1f29fda8b50f9
whoami847 <mrbeastbangla069@gmail.com>
2025-04-05 07:03:55 +0600

    Update delete_files.py

--- bot/handlers/delete_files.py
+++ bot/handlers/delete_files.py
@@ -1,19 +1,18 @@
-import os
-import schedule
-import time
-from pathlib import Path
-from config import Config
+import asyncio
+from datetime import datetime, timedelta
+from config import config
+from database import db
 
-def cleanup_files():
-    path = Path(Config.STORAGE_PATH)
-    for file in path.glob("*"):
-        # Delete files older than 24 hours
-        if time.time() - file.stat().st_mtime > 24 * 3600:
-            os.remove(file)
-            print(f"Deleted: {file.name}")
-
-def start_cleanup_scheduler():
-    schedule.every().hour.do(cleanup_files)
+async def auto_delete():
     while True:
-        schedule.run_pending()
-        time.sleep(1)
+        files_col = await db.get_collection("files")
+        await files_col.delete_many({
+            "expire_at": {"$lt": datetime.now()}
+        })
+        await asyncio.sleep(config.AUTO_DELETE_TIME * 60)
+
+async def delete_file_handler(_, message: Message):
+    if message.from_user.id in config.ADMINS:
+        file_id = message.command[1]
+        await db.files.delete_one({"_id": file_id})
+        await message.reply("File deleted!")
