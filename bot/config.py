b2550d77e90e60cb0aec297314b4145649565b4b
whoami847 <mrbeastbangla069@gmail.com>
2025-04-05 11:06:27 +0600

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,13 +1,13 @@
-import os
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Config(BaseSettings):
     API_ID: int = 28774737
     API_HASH: str = "851190ab85bb0b6dd547fff8e3c35b73"
     BOT_TOKEN: str = "7671316803:AAFiG_mhL1wy7T1cTB-aL_ktjVAO0vwKvhU"
-    # Add other config values...
-
-    class Config:
-        env_file = ".env"
+    
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore"
+    )
 
 config = Config()
