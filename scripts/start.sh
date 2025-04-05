7ec31885303e6ce9c9033d5174cac25ec7797beb
whoami847 <mrbeastbangla069@gmail.com>
2025-04-05 10:55:09 +0600

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Start FastAPI server
-uvicorn web.routes:router --host 0.0.0.0 --port 8080 &
+# Start web server
+uvicorn web.routes:app --host 0.0.0.0 --port 8080 &
 
-# Start Telegram bot
+# Start bot
 python3 -m bot.main_handler &
 
-# Start cleanup scheduler
-python3 -m bot.handlers.delete_files &
-
-# Keep container running
+# Keep container alive
 wait
