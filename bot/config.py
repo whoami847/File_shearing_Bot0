8074cd238409a4440808d16304465995a5fd558b
whoami847 <mrbeastbangla069@gmail.com>
2025-04-05 13:15:18 +0600

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,4 +1,5 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import List
 
 class Config(BaseSettings):
     # Required Fields (Telegram)
@@ -8,19 +9,20 @@ class Config(BaseSettings):
     
     # Optional Fields with Default Values
     OWNER_ID: int = 7282066033
-    ADMINS: list[int] = [7282066033, 6564441490]
+    ADMINS: List[int] = [7282066033, 6564441490]
     CHANNEL_ID: int = -1002644513465
-    FSUB_CHANNELS: list[int] = [-1002644513465, -1002656647177]
+    FSUB_CHANNELS: List[int] = [-1002644513465, -1002656647177]
     DATABASE_URL: str = "mongodb+srv://Anime1:Anime1@cluster0.030ybld.mongodb.net/Cluster0?retryWrites=true&w=majority&appName=Cluster0"
     DATABASE_NAME: str = "Cluster0"
     AUTO_DELETE_TIME: int = 1200  # Minutes
     PROTECT_CONTENT: bool = False
     PORT: int = 8080
 
-    # Pydantic v2 Configuration
+    # Pydantic Configuration
     model_config = SettingsConfigDict(
         env_file=".env",
-        extra="ignore"  # Allow extra fields in .env
+        env_file_encoding="utf-8",
+        extra="ignore"
     )
 
 config = Config()
