8acfad37f2981d2d8b97b29530bb875243e21bd0
whoami847 <mrbeastbangla069@gmail.com>
2025-04-05 07:07:10 +0600

    Update ratelimiter.py

--- bot/utils/ratelimiter.py
+++ bot/utils/ratelimiter.py
@@ -1,13 +1,36 @@
-from fastapi import HTTPException
-from fastapi import Request
-from fastapi import status
-from slowapi import Limiter
-from slowapi.util import get_remote_address
+from pyrogram import filters
+from database import db
+from datetime import datetime, timedelta
 
-limiter = Limiter(key_func=get_remote_address)
+class RateLimiter:
+    def __init__(self, limit: int, window: int):
+        self.limit = limit
+        self.window = window
 
-def rate_limit_exceeded_handler(request: Request, exc: Exception):
-    raise HTTPException(
-        status_code=status.HTTP_429_TOO_MANY_REQUESTS,
-        detail="Too many requests"
-    )
+    async def check(self, user_id: int):
+        users_col = await db.get_collection("ratelimit")
+        user = await users_col.find_one({"_id": user_id})
+        
+        if not user:
+            await users_col.insert_one({
+                "_id": user_id,
+                "count": 1,
+                "last_update": datetime.now()
+            })
+            return True
+            
+        if (datetime.now() - user["last_update"]) > timedelta(seconds=self.window):
+            await users_col.update_one(
+                {"_id": user_id},
+                {"$set": {"count": 1, "last_update": datetime.now()}}
+            )
+            return True
+            
+        if user["count"] >= self.limit:
+            return False
+            
+        await users_col.update_one(
+            {"_id": user_id},
+            {"$inc": {"count": 1}}
+        )
+        return True
