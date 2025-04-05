ad4f6c3cab15ba05c71384b23681bf352dec7a31
whoami847 <mrbeastbangla069@gmail.com>
2025-04-05 11:38:54 +0600

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# Start web server
-uvicorn bot.web.routes:router --host 0.0.0.0 --port 8080 &
-
-# Start Telegram bot
-python3 -m bot.main_handler &
-
-# Keep container alive
-wait
+# Start the main application process
+exec python3 -m bot.main_handler
