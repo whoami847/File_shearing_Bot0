958f5c5b719022d0e221df502b7e94761f1b4155
whoami847 <mrbeastbangla069@gmail.com>
2025-04-05 06:26:49 +0600

    Update database.py

--- bot/database.py
+++ bot/database.py
@@ -1,15 +1,12 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from config import Config
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import config
 
-engine = create_engine(Config.DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+class Database:
+    def __init__(self):
+        self.client = AsyncIOMotorClient(config.DATABASE_URL)
+        self.db = self.client[config.DATABASE_NAME]
+    
+    async def get_collection(self, name: str):
+        return self.db[name]
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+db = Database()
